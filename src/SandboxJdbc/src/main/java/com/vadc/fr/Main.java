002da87405bad23081a56825a12a4eb013ee3b4b
Christian Popescu <christian.popescu@outlook.com>
2020-11-15 19:40:24 +0100

    jdbc with Oracle connection working

--- src/SandboxJdbc/src/main/java/com/vadc/fr/Main.java
+++ src/SandboxJdbc/src/main/java/com/vadc/fr/Main.java
@@ -1,8 +1,15 @@
 package com.vadc.fr;
+import com.vadc.fr.db.DbUtilsFactory;
+import com.vadc.fr.db.SqlRunner;
+import oracle.jdbc.driver.OracleDriver;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        SqlRunner sqlRunner =
+                new SqlRunner( DbUtilsFactory.CreateDataSource(new OracleDriver(), "oracle.jdbc.driver.OracleDriver",
+        "sandbox", "sandbox","jdbc:oracle:thin:@192.168.5.129:1521:orcl"));
+        sqlRunner.test();
     }
 }
--- src/SandboxJdbc/src/main/java/com/vadc/fr/db/SqlRunner.java
+++ src/SandboxJdbc/src/main/java/com/vadc/fr/db/SqlRunner.java
@@ -1,5 +1,9 @@
 package com.vadc.fr.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.sql.DataSource;
 
 public class SqlRunner {
@@ -9,6 +13,27 @@ public class SqlRunner {
     public SqlRunner (DataSource pDataSource)
     {
         dataSource = pDataSource;
+    }
+
+    // method to delete
+    public void test()
+    {
+        try (Connection conn = dataSource.getConnection())
+        {
+            String command = "select * from USER_TABLES";
+            PreparedStatement statement = conn.prepareStatement(command);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next())
+            {
+                System.out.println(rs.getString(1));
+            }
+
+        }
+        catch (SQLException se)
+        {
+            System.out.println("Sql Exception: " + se.getMessage());
+
+        }
     }
 
 
